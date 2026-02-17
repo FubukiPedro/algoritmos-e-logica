5c78627fae65f1350849b6c6d9c6daa095dd7a51
Fubuki <70610188+FubukiPedro@users.noreply.github.com>
2026-02-16 22:57:34 -0300

    Adiciona exercícios de lógica em C#
    Exercícios de lógica de programação desenvolvidos em C#, organizados por estruturas sequencial, condicional, repetitiva, vetores e matrizes.

---
+++ logica-csharp/estrutura-sequencial/idades/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace idades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String nome1, nome2;
+            int idade1, idade2;
+            double media;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade2 = int.Parse(Console.ReadLine());
+
+            media = (double)(idade1 + idade2) / 2;
+            Console.WriteLine("A idade media de " + nome1 + " e " + nome2 + " eh de " + media.ToString("F1", CI) + " anos.");
+
+        }
+    }
+}
---
+++ logica-csharp/vetores/somaVetor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+namespace somaVetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            Console.Write("VALORES: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(vet[i].ToString("F1", CI) + " ");
+            }
+            Console.WriteLine();
+
+            double soma, media;
+
+            soma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                soma = soma + vet[i];
+            }
+            Console.WriteLine("SOMA: " + soma.ToString("F2", CI));
+
+            media = soma / n;
+            Console.WriteLine("MEDIA: " + media.ToString("F2", CI));
+
+        }
+    }
+}
